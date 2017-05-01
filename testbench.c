baa6ed235f5e9eb725a980324f9922fc4bc2d742
abhi007 <abhi007@vt.edu>
2017-04-30 23:15:06 -0400

    Added strcmp functionality and code optimization

--- testbench.c
+++ testbench.c
@@ -13,202 +13,213 @@ float getTimeDiff(struct timeval t0, struct timeval t1);
 
 int main(void)
 {
-	int ret, i;
+	int ret, i, misMatch;
 	char buffer[128];
 	buffer[0] = '\0';
 	struct timeval t0, t1;
 	float timeDiff = 0;
 	char key_A[4096], val_A[4096], keyChar[128], valChar[128];
+	char keyContainer[4096][256], valContainer[4096][256];
 
 	/* first let's format the partition to make sure we operate on a 
 	 * well-known state */
 	ret = kvlib_format();
-	printf("@@@ Formatting done:\n");
-	// printf(" returns: %d (should be 0)\n", ret);
-
-	/* "set" operation test */
-	// ret = kvlib_set("key1", "val1");
-	// printf("Insert 1 (key1, val1):\n");
-	// printf(" returns: %d (should be 0)\n", ret);
-
-	// ret = kvlib_set("key2", "val2");
-	// printf("Insert 2 (key2, val2):\n");
-	// printf(" returns: %d (should be 0)\n", ret);
-
-	/* Now let's fill an entire block lplus an additional page (we assume 
-	 * there are 64 pages per block) */
-
-
+	if (ret == 0)
+		printf("Formatting done:\n");
+	else
+		printf("Error in Formatting\n");
+	
 	// Write latency to fill a block with small keys
-	ret = 0;
-	gettimeofday(&t0,NULL);
+
+	ret=0;
+	
 	for (i = 1; i < 65; i++) 
 	{
 		char key[128], val[128];
 		sprintf(key, "key%d", i);
 		sprintf(val, "val%d", i);
-		ret += kvlib_set(key, val);
+		strcpy(keyContainer[i], key);
+		strcpy(valContainer[i], val);	
 	}
+
+	gettimeofday(&t0,NULL);
+	for (i = 0; i < 65; i++)
+		ret += kvlib_set(keyContainer[i], valContainer[i]);
 	gettimeofday(&t1,NULL);
-	
+
 	timeDiff = getTimeDiff(t0,t1);
-	printf("@@@ Write latency for small keys:\t %f \tus\n", timeDiff);
-	// printf(" returns: %d (should be 0)\n", ret);
-
-
+	if (ret == 0)
+		printf("Write latency for small keys:\t %f \tus\n", timeDiff);
+	else
+		printf("Error in writing small keys...\n");
+	
+	
 	// Read latency for small keys
 
 	ret = 0;
+	misMatch = 0;
+
 	gettimeofday(&t0,NULL);
-	for (i = 1; i < 65; i++) {
-		char key[128];
-		sprintf(key, "key%d", i);
-		ret += kvlib_get(key, buffer);
+	for(i=1; i<65; i++)
+	{
+		ret += kvlib_get(keyContainer[i], buffer);
+		if(strcmp(buffer, valContainer[i]) != 0)
+			misMatch++;
 	}
 	gettimeofday(&t1,NULL);
 	timeDiff = getTimeDiff(t0,t1);
-	printf("@@@ Read latency for small keys:\t %f \tus\n", timeDiff);
-	// printf(" returns: %d (should be 0)\n", ret);
-
+	
+	if (ret == 0)
+		printf("Read latency for small keys:\t %f \tus \t misMatch: %d\n", timeDiff, misMatch);
+	else
+		printf("Error in reading small keys...\n");
+	
 	// Delete latency for small keys
 	ret = 0;
+
 	gettimeofday(&t0,NULL);
-	for (i = 1; i < 65; i++) {
-		char key[128];
-		sprintf(key, "key%d", i);
-		ret += kvlib_del(key);
-	}
+	for(i=1; i<65;i++)
+		ret += kvlib_del(keyContainer[i]);
+	
 	gettimeofday(&t1,NULL);
 	timeDiff = getTimeDiff(t0,t1);
-	printf("@@@ Delete latency for small keys:\t %f \tus\n", timeDiff);
-	// printf(" returns: %d (should be 0)\n", ret);
 
+	if (ret == 0)
+		printf("Delete latency for small keys:\t %f \tus\n", timeDiff);
+	else
+		printf("Error in Deleting small keys...\n");
 
-	// @@@ Large keys:
+	// @@@ Writing Large keys:
 	ret = 0;
-	sprintf(key_A, "keyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy%d", 0);
-	sprintf(val_A, "valueeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee%d", 0);
+	sprintf(key_A, "keyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy%d", 0);
+	sprintf(val_A, "valueeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee%d", 0);
 
-	gettimeofday(&t0,NULL);
 	for (i = 1; i < 65; i++) 
 	{
 		sprintf(keyChar, "%d", i);
 		sprintf(valChar, "%d", i);
 		strcat(key_A, keyChar);
 		strcat(val_A, valChar);
-		ret += kvlib_set(key_A, val_A);
+		strcpy(keyContainer[i], key_A);
+		strcpy(valContainer[i], val_A);
+	}
+
+	gettimeofday(&t0,NULL);
+	for(i=1; i<65; i++)
+		ret += kvlib_set(keyContainer[i], valContainer[i]);
+	gettimeofday(&t1,NULL);
+	timeDiff = getTimeDiff(t0,t1);
+
+	if (ret == 0)
+		printf("Write latency for large keys:\t %f \tus\n", timeDiff);
+	else
+		printf("Error in writing large keys...\n");
+
+	// Read latency for large keys
+	ret = 0;
+	misMatch = 0;
+
+	gettimeofday(&t0,NULL);
+	for(i=1; i<65; i++)
+	{
+		ret += kvlib_get(keyContainer[i], buffer);
+		if(strcmp(buffer, valContainer[i]) != 0)
+			misMatch++;
 	}
 	gettimeofday(&t1,NULL);
 	timeDiff = getTimeDiff(t0,t1);
-	printf("@@@ Write latency for large keys:\t %f \tus\n", timeDiff);
-	// printf("returns: %d (should be 0)\n", ret);
 
-	// Read latency for stress testing
+	if (ret == 0)
+		printf("Read latency for large keys:\t %f \tus \t misMatch: %d\n", timeDiff, misMatch);
+	else
+		printf("Error in reading large keys...\n");
 
+
+	//Delete latency for large keys
 	ret = 0;
-	//char key_A[128], val_A[128], keyChar[128], valChar[128];
-
-	sprintf(key_A, "key%d", 0);
-	sprintf(val_A, "val%d", 0);
 		
 	gettimeofday(&t0,NULL);
 	for (i = 1; i < 65; i++) 
-	{
-		sprintf(keyChar, "%d", i);
-		strcat(key_A, keyChar);
-		ret += kvlib_get(key_A, buffer);
-	}
+		ret += kvlib_del(keyContainer[i]);
 	gettimeofday(&t1,NULL);
 	timeDiff = getTimeDiff(t0,t1);
-	printf("@@@ Read latency for large keys:\t %f \tus\n", timeDiff);
-	// printf("returns: %d (should be 0)\n", ret);
 
-	//Delete latency for stress testing
-	ret = 0;
-	//char key_A[128], val_A[128], keyChar[128], valChar[128];
-
-	sprintf(key_A, "key%d", 0);
-	sprintf(val_A, "val%d", 0);
-		
-	gettimeofday(&t0,NULL);
-	for (i = 1; i < 65; i++) 
-	{
-		sprintf(keyChar, "%d", i);
-		strcat(key_A, keyChar);
-		ret += kvlib_del(key_A);
-	}
-	gettimeofday(&t1,NULL);
-	timeDiff = getTimeDiff(t0,t1);
-	printf("@@@ Delete latency for large keys:\t %f \tus\n", timeDiff);
-	// printf("returns: %d (should be 0)\n", ret);
-
-
+	if (ret == 0)
+		printf("Delete latency for large keys:\t %f \tus\n", timeDiff);
+	else
+		printf("Error in deleting large keys...\n");
+	
 	// @@@ Stress testing: Varying the size of key and value
 	ret = 0;
-	
 	sprintf(key_A, "key%d", 0);
 	sprintf(val_A, "val%d", 0);
 		
-	gettimeofday(&t0,NULL);
+	
 	for (i = 1; i < 500; i++) 
 	{
 		sprintf(keyChar, "%d", i);
 		sprintf(valChar, "%d", i);
 		strcat(key_A, keyChar);
 		strcat(val_A, valChar);
-		//printf("@@@ key inserted : %s\n", key_A);
-		ret += kvlib_set(key_A, val_A);
-		//printf("@@@ returns: %d (should be 0)\n", ret);
+		strcpy(keyContainer[i], key_A);
+		strcpy(valContainer[i], val_A);		
 	}
+	gettimeofday(&t0,NULL);
+	for(i=1;i<65; i++)
+		ret += kvlib_set(keyContainer[i], valContainer[i]);	
+
 	gettimeofday(&t1,NULL);
 	timeDiff = getTimeDiff(t0,t1);
-	printf("@@@ Write latency for varying key/val:\t %f \tus\n", timeDiff);
-	// printf("returns: %d (should be 0)\n", ret);
+	
+	if (ret == 0)
+		printf("Write latency for varying key/val:\t %f \tus\n", timeDiff);
+	else
+		printf("Error in writing varying keys...\n");
 
 	// Read latency for stress testing
-
 	ret = 0;
-	//char key_A[128], val_A[128], keyChar[128], valChar[128];
+	misMatch=0;
 
-	sprintf(key_A, "key%d", 0);
-	sprintf(val_A, "val%d", 0);
-		
 	gettimeofday(&t0,NULL);
 	for (i = 1; i < 65; i++) 
 	{
-		sprintf(keyChar, "%d", i);
-		strcat(key_A, keyChar);
-		ret += kvlib_get(key_A, buffer);
+		ret += kvlib_get(keyContainer[i], buffer);
+		if(strcmp(buffer, valContainer[i]) != 0)
+			misMatch++;
 	}
 	gettimeofday(&t1,NULL);
 	timeDiff = getTimeDiff(t0,t1);
-	printf("@@@ Read latency for varying key/val:\t %f \tus\n", timeDiff);
-	// printf("returns: %d (should be 0)\n", ret);
+
+	if (ret == 0)
+		printf("Read latency for varying key/val:\t %f \tus \t misMatch: %d\n", timeDiff, misMatch);
+	else
+		printf("Error in reading varying keys...\n");
 
 	//Delete latency for stress testing
-
 	ret = 0;
-	//char key_A[128], val_A[128], keyChar[128], valChar[128];
-
-	sprintf(key_A, "key%d", 0);
-	sprintf(val_A, "val%d", 0);
-		
 	gettimeofday(&t0,NULL);
 	for (i = 1; i < 65; i++) 
-	{
-		sprintf(keyChar, "%d", i);
-		strcat(key_A, keyChar);
-		ret += kvlib_del(key_A);
-	}
+		ret += kvlib_del(keyContainer[i]);
 	gettimeofday(&t1,NULL);
 	timeDiff = getTimeDiff(t0,t1);
-	printf("@@@ Delete latency for varying key/val:\t %f \tus\n", timeDiff);
-	// printf("returns: %d (should be 0)\n", ret);
+
+	if (ret == 0)
+		printf("Delete latency for varying key/val:\t %f \tus\n", timeDiff);
+	else
+		printf("Error in deleting varying keys...\n");
+
+
+	//Let's format again...
+	ret = kvlib_format();
+	if (ret == 0)
+		printf("Formatting done:\n");
+	else
+		printf("Error in Formatting\n");
 
 	// Simultaneous read/write/delete
 	// Read from key0 to key64 and write to key64 to key128
 	ret = 0;
-	for (i=0; i< 65; i++)
+	for (i=1; i< 65; i++)
 	{
 		sprintf(key_A, "key%d", i);
 		ret += kvlib_get(key_A, buffer);
